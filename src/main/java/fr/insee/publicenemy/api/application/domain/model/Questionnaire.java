bd342b686cb2e00218235274c05f0e340afe8548
David Darras <david.darras@insee.fr>
2023-02-22 17:26:12 +0100

    chore: delete useless imports

--- src/main/java/fr/insee/publicenemy/api/application/domain/model/Questionnaire.java
+++ src/main/java/fr/insee/publicenemy/api/application/domain/model/Questionnaire.java
@@ -9,10 +9,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-
 @Getter
 @Setter
 @AllArgsConstructor
