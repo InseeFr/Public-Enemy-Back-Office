d39e50ccced26c8d3cc5c30d4105b3a246bfc797
David Darras <david.darras@insee.fr>
2023-05-04 10:44:41 +0200

    docs: avoid dangling javadoc comments

--- src/main/java/fr/insee/publicenemy/api/application/exceptions/SurveyUnitsSpecificValidationException.java
+++ src/main/java/fr/insee/publicenemy/api/application/exceptions/SurveyUnitsSpecificValidationException.java
@@ -6,11 +6,11 @@ import lombok.Getter;
 import java.io.Serial;
 import java.util.List;
 
-@Getter
 /**
  * When validating survey units csv data attributes against variables types from a questionnaire model, this exception is throwed
  * if validation has failed on specific attributes
  */
+@Getter
 public class SurveyUnitsSpecificValidationException extends Exception {
     @Serial
     private static final long serialVersionUID = -1619203216771899549L;
