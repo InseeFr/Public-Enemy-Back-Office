b99e91444397a4fe424ad3b2af1feaf0d891840e
Laurent Caouissin <caouissin.laurent@gmail.com>
2024-06-26 15:37:10 +0200

    feat; create new service to compute Url of visu

--- src/main/java/fr/insee/publicenemy/api/controllers/dto/SurveyUnitRest.java
+++ src/main/java/fr/insee/publicenemy/api/controllers/dto/SurveyUnitRest.java
@@ -1,17 +1,3 @@
 package fr.insee.publicenemy.api.controllers.dto;
 
-import fr.insee.publicenemy.api.application.domain.model.surveyunit.SurveyUnit;
-import fr.insee.publicenemy.api.application.domain.model.surveyunit.SurveyUnitIdentifierHandler;
-
-public record SurveyUnitRest(String id, int displayableId) {
-    /**
-     * @param surveyUnit survey unit model
-     * @return a new survey unit rest from the survey unit model
-     */
-    public static SurveyUnitRest fromModel(SurveyUnit surveyUnit) {
-        String queenIdentifier = surveyUnit.id();
-        // split the id to get rid of the questionnaire id part for frontend
-        SurveyUnitIdentifierHandler identifierHandler = new SurveyUnitIdentifierHandler(queenIdentifier);
-        return new SurveyUnitRest(queenIdentifier, identifierHandler.getSurveyUnitIdentifier());
-    }
-}
+public record SurveyUnitRest(String id, int displayableId, String url) {}
