a063d616411982a864f632ce37df9d1aaab7cdac
Laurent Caouissin <caouissin.laurent@gmail.com>
2025-07-29 17:47:39 +0200

    feat: create personalizationState

---
+++ src/main/java/fr/insee/publicenemy/api/application/domain/model/PersonalizationState.java
@@ -0,0 +1,10 @@
+package fr.insee.publicenemy.api.application.domain.model;
+
+/**
+ * State used for synchronisation with queen
+ */
+public enum PersonalizationState {
+    STARTED,
+    ERROR,
+    COMPLETED;
+}
