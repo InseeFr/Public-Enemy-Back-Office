ea08b8f4634db28e11d6eb06001153bce34fe211
Laurent Caouissin <38245508+laurentC35@users.noreply.github.com>
2025-08-20 13:34:01 +0200

    feat: more explicit message invalid json (#150)

--- src/test/java/fr/insee/publicenemy/api/infrastructure/json/InterrogationJsonServiceTest.java
+++ src/test/java/fr/insee/publicenemy/api/infrastructure/json/InterrogationJsonServiceTest.java
@@ -1,14 +1,12 @@
 package fr.insee.publicenemy.api.infrastructure.json;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.insee.publicenemy.api.application.domain.model.Mode;
 import fr.insee.publicenemy.api.application.domain.model.PersonalizationMapping;
 import fr.insee.publicenemy.api.application.domain.model.interrogation.*;
+import fr.insee.publicenemy.api.application.exceptions.ServiceException;
 import fr.insee.publicenemy.api.application.ports.I18nMessagePort;
 import fr.insee.publicenemy.api.infrastructure.json.exceptions.InterrogationJsonNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -17,11 +15,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.*;
 
+import static fr.insee.publicenemy.api.infrastructure.json.InterrogationJsonService.simplifyMessage;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class InterrogationJsonServiceTest {
@@ -47,6 +48,22 @@ class InterrogationJsonServiceTest {
         List<Interrogation> interrogations = service.initInterrogations(surveyUnitData, questionnaireModelId);
 
         assertEquals(1, interrogations.size());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"invalid"})
+    void onGetInvalidJsonShouldThrowTechnicalError(String dataPath) throws IOException {
+        String questionnaireModelId = "dummy";
+
+        String resourcePath = String.format("src/test/resources/%s.json", dataPath);
+        File file = new File(resourcePath);
+        byte[] surveyUnitData = Files.readAllBytes(file.toPath());
+
+        when(messageService.getMessage(eq("validation.json.malform.error"), any())).thenAnswer(
+                invocation -> simplifyMessage(invocation.getArgument(1)));
+
+        ServiceException exception = assertThrows(ServiceException.class, () -> service.initInterrogations(surveyUnitData, questionnaireModelId));
+        assertEquals("Unexpected character (']' (code 93)): was expecting double-quote to start field name (line: 4, column: 3)", exception.getMessage());
     }
 
     @ParameterizedTest
