a623d98f3467568d2a39f4443897ec82cdb58336
Laurent Caouissin <caouissin.laurent@gmail.com>
2025-09-03 10:16:02 +0200

    feat: remove useless dto

--- src/main/java/fr/insee/publicenemy/api/application/ports/QueenServicePort.java
+++ src/main/java/fr/insee/publicenemy/api/application/ports/QueenServicePort.java
@@ -5,7 +5,6 @@ import fr.insee.publicenemy.api.application.domain.model.Questionnaire;
 import fr.insee.publicenemy.api.application.domain.model.QuestionnaireModel;
 import fr.insee.publicenemy.api.application.domain.model.interrogation.Interrogation;
 import fr.insee.publicenemy.api.infrastructure.queen.dto.InterrogationDto;
-import fr.insee.publicenemy.api.infrastructure.queen.dto.InterrogationSurveyUnitDto;
 import fr.insee.publicenemy.api.infrastructure.queen.exceptions.CampaignNotFoundException;
 
 import java.util.List;
